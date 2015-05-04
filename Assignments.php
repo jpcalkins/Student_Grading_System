5124649021fc63753e1e4acc33a86772a48d6b80
Jacob Calkins <jpcalk@gmail.com>
2015-05-03 19:45:08 -0500

    Added ability for Instructors to see student's grades on assignments

--- Assignments.php
+++ Assignments.php
@@ -7,39 +7,76 @@
  */
 include 'Project1.php';
 session_start();
-if(count($_POST)==0) {
-    css();
-    checkSession(array('student'));
-    echo "<script src='Project1.js'></script><script src='jquery-1.11.2.min.js'></script>";
-    $userId = $_SESSION['userId'];
-    $dropdown = "Select Class:<br>
-        <select name='classId' onchange='showTable(this.value)'>";
-    $query = "SELECT className, classNum, classId FROM Classes NATURAL JOIN Takes WHERE userId={$_SESSION['userId']}";
-    $result = sqlLogin()->query($query);
-    foreach ($result as $row) {
-        $dropdown .= "\r\n<option value='{$row['classId']}'>{$row['classId']}-{$row['classNum']}-{$row['className']}</option>";
+if($_SESSION['role'] == 'student') {
+    if (count($_POST) == 0) {
+        css();
+        checkSession(array('student'));
+        echo "<script src='Project1.js'></script><script src='jquery-1.11.2.min.js'></script>";
+        $userId = $_SESSION['userId'];
+        $dropdown = "Select Class:<br>
+            <select name='classId' onchange='showTable(this.value)'>";
+        $query = "SELECT className, classNum, classId FROM Classes NATURAL JOIN Takes WHERE userId={$_SESSION['userId']}";
+        $result = sqlLogin()->query($query);
+        foreach ($result as $row) {
+            $dropdown .= "\r\n<option value='{$row['classId']}'>{$row['classId']}-{$row['classNum']}-{$row['className']}</option>";
+        }
+        $dropdown .= "\r\n</select>";
+        echo "{$dropdown}<br<br><br><div id='tablePrint'></div>";
+    }elseif(count($_POST) == 1) {
+        $query = "SELECT SUM(points), SUM(numPoints)
+    FROM  AssignmentGrades NATURAL JOIN Assignments
+    WHERE studentId = {$_SESSION['userId']} AND Assignments.classId = {$_POST['classId']}";
+        $result = sqlLogin()->query($query);
+        $row = $result->fetch_row();
+        $grade = (($row[0] / $row[1]) * 100);
+        $query = "SELECT Assignments.assignmentName, concat(points, '/', numPoints) AS Grade
+    FROM  AssignmentGrades NATURAL JOIN Assignments
+    WHERE studentId = {$_SESSION['userId']} AND Assignments.classId = {$_POST['classId']}";
+        $result = sqlLogin()->query($query);
+        $row = $result->fetch_row();
+        $jsonString = array();
+        foreach ($result as $row) {
+            array_push($jsonString, array($row['assignmentName'], $row['Grade'], $grade));
+        }
+        $jsonString = json_encode($jsonString);
+        echo $jsonString;
+    }
+}elseif($_SESSION['role'] == 'instructor'){
+    if(count($_POST) == 0){
+        css();
+        checkSession(array('instructor'));
+        echo "<script src='Project1.js'></script><script src='jquery-1.11.2.min.js'></script>";
+        $dropdown = "Select Class:<br>
+                <select name='classId' onchange='showList(this.value)'>";
+        $query = "SELECT classId, classNum, className FROM Classes NATURAL JOIN Teaches WHERE userId={$_SESSION['userId']}";
+        $result = sqlLogin()->query($query);
+        foreach ($result as $row) {
+            $dropdown .= "\r\n<option value='{$row['classId']}'>{$row['classId']}-{$row['classNum']}-{$row['className']}</option>";
 
+        }
+        $dropdown .= "\r\n</select>";
+        echo "{$dropdown}<br<br><br><div id='listPrint'></div><div id='tablePrint'></div>";
+    }elseif(isset($_POST['classId'])){
+        $query = "SELECT assignmentName FROM Assignments WHERE classId={$_POST['classId']}";
+        $result = sqlLogin()->query($query);
+        $jsonString = array();
+        foreach ($result as $row) {
+            array_push($jsonString, array($row['assignmentName']));
+        }
+        $jsonString = json_encode($jsonString);
+        echo $jsonString;
+    }elseif(isset($_POST['assignmentName'])){
+        $query = "SELECT userId, name, concat(points, '/', numPoints) AS Grade
+          FROM Assignments NATURAL JOIN AssignmentGrades JOIN Students
+            ON AssignmentGrades.studentId = Students.userId
+          WHERE assignmentName='{$_POST['assignmentName']}'";
+        $result = sqlLogin()->query($query);
+        $row = $result->fetch_array(MYSQL_ASSOC);
+        $jsonString = array();
+        foreach ($result as $row){
+            array_push($jsonString, array($row['userId'], $row['name'], $row['Grade']));
+        }
+        $jsonString = json_encode($jsonString);
+        echo $jsonString;
     }
-    $dropdown .= "\r\n</select>";
-    echo "{$dropdown}<br<br><br><div id='tablePrint'></div>";
 }
-if(count($_POST) == 1){
-    $query = "SELECT SUM(points), SUM(numPoints)
-FROM  AssignmentGrades NATURAL JOIN Assignments
-WHERE studentId = {$_SESSION['userId']} AND Assignments.classId = {$_POST['classId']}";
-    $result = sqlLogin()->query($query);
-    $row = $result->fetch_row();
-    $grade = (($row[0]/$row[1])*100);
-    $query = "SELECT Assignments.assignmentName, concat(points, '/', numPoints) AS Grade
-FROM  AssignmentGrades NATURAL JOIN Assignments
-WHERE studentId = {$_SESSION['userId']} AND Assignments.classId = {$_POST['classId']}";
-    $result = sqlLogin()->query($query);
-    $row = $result->fetch_row();
-    $jsonString = array();
-    foreach($result as $row) {
-        array_push($jsonString, array($row['assignmentName'], $row['Grade'], $grade));
-    }
-    $jsonString = json_encode($jsonString);
-    echo $jsonString;
-}
-home();
--- Project1.js
+++ Project1.js
@@ -7,7 +7,6 @@ function showTable(classId){
         url: 'Assignments.php',
         method: 'POST',
         success: function(msg) {
-            alert(msg);
             try{
                 msg = JSON.parse(msg);
             }catch(e){
@@ -22,11 +21,55 @@ function showTable(classId){
             }
             table += "</table>";
             document.getElementById('tablePrint').innerHTML = table;
-        },
-        error: function(xhr, status, error) {
-            // check status && error
+        }
+    });
+}
+function showList(classId){
+    $.ajax({
+        data: 'classId=' + classId,
+        url: 'Assignments.php',
+        method: 'POST',
+        success: function (msg) {
             alert(msg);
-            alert("Status: " + status + " Error: " + error);
+            try {
+                msg = JSON.parse(msg);
+            } catch (e) {
+                alert("Error generating table");
+                return;
+            }
+            var list = "Select Assignment:<br><select name='assignmentName' onchange='showAssignmentTable(this.value)'>";
+            for(var i=0; i<msg.length; i++){
+                list += "\r\n<option value='"+msg[i]+"'>"+msg[i]+"</option>";
+            }
+            document.getElementById('listPrint').innerHTML = list;
+        }
+    });
+}
+function showAssignmentTable(assignmentName){
+    $.ajax({
+        data: 'assignmentName=' + assignmentName,
+        url: 'Assignments.php',
+        method: 'POST',
+        success: function (msg) {
+            alert(msg);
+            if(msg.length == 0){
+                alert("No assignments in that class");
+                return;
+            }
+            try {
+                msg = JSON.parse(msg);
+            } catch (e) {
+                alert("Error generating table");
+                return;
+            }
+            var table = "<table border='2' cellPadding='3'><tr><th colspan='3'>Grades for " + assignmentName +"</th></tr><tr>";
+            for(var i = 0; i<msg.length; i++){
+                table += "<td>" + msg[i][0] + "</td>";
+                table += "<td>" + msg[i][1] + "</td>";
+                table += "<td>" + msg[i][2] + "</td></tr>";
+            }
+            table += "</table>";
+            document.getElementById('tablePrint').innerHTML = table;
         }
     });
 }
